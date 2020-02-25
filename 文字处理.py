d4a7ee74684974b26cc5b947c53a8229af027109
jiajiayao <jiajiayaoabc@qq.com>
2020-02-25 10:30:34 +0800

    增加错误校验，完善代码

--- 文字处理.py
+++ 文字处理.py
@@ -2,6 +2,8 @@ from aip import AipOcr
 
 import os
 
+accept=[] #正确
+unaccept=[]#错误
 
 def work(item):
     """ 你的 APPID AK SK  图2的内容"""
@@ -25,21 +27,42 @@ def work(item):
     results = client.general(image)["words_result"]  # 还可以使用身份证驾驶证模板，直接得到字典对应所需字段
     newName=''
     rule='姓名'
+    wrong='返程健康登记'
+    extra=''
+    isextra=0
+
+    
+
     for result in results:
         text = result["words"]
-        if(rule in text):
+        if (wrong in text):
+            extra='非安康码'
+            isextra=1
+        elif(rule in text):
             newName=text
             break
 
     print(newName)
-    os.rename('801/801/'+item,'801/801/'+ newName.replace(':','--')+'.jpg')
+    if(isextra):
+        unaccept.append(text.replace('姓名:','')) 
+    else:
+        accept.append(text.replace('姓名:','')) 
+    
+    os.rename('801/801/'+item,'801/801/'+ extra+newName.replace(':','--').replace('*','')+'.jpg')#非法字符处理
 if __name__ == '__main__':
     filePath = '801/801'
     names = os.listdir(filePath)
     #print(name)
     for item in names:
-        work(item)
-
-
+        try:
+            work(item)
+        except:
+            print('姓名包含非法字符或者有重复图片：',item)
+            continue
+    
+    print('正确')
+    print(accept)
+    print('错误')
+    print(unaccept)
 
 
